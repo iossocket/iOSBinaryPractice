28f141df361d6f0fa12c3a90a2ae3298490c9f9c
iossocket <avx302@gmail.com>
2017-07-06 13:39:23 +0800

    modify framework

---
+++ Products/iOSBinaryPractice.framework/Headers/AClass.h
@@ -0,0 +1,15 @@
+//
+//  AClass.h
+//  iOSBinaryPractice
+//
+//  Created by XueliangZhu on 06/07/2017.
+//  Copyright © 2017 ThoughtWorks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AClass : NSObject
+
++ (void)doSomething;
+
+@end
